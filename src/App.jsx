0e2cbf1e36e8e03613a9ca3d1c1e15ab73fe85f0
ecaliskan37 <ebru-1-mat@hotmail.com>
2025-08-01 02:57:40 +0300

    ddkdkd

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './styles.css'
 import TopText from './components/TopText'
 
 export default function App() {
   const [filesToUpload, setFilesToUpload] = useState([])
+  const ref = useRef()
 
   function handleSubmit(e) {
     e.preventDefault()
     filesToUpload.forEach((file) => console.log(file))
+    ref.current.value = ''
   }
 
   const handleFileChange = (e) => {
+    console.log(e)
     if (e.target.files) {
-      setFilesToUpload([...filesToUpload, e.target.files])
+      setFilesToUpload([...filesToUpload, ...e.target.files])
     }
   }
 
@@ -20,6 +23,7 @@ export default function App() {
     <form onSubmit={handleSubmit}>
       <TopText />
       <input
+        ref={ref}
         type="file"
         accept=".jpg, .jpeg, .png, .pdf"
         multiple
